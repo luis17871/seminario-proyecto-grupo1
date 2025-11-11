1b721c52a09567ac481f1e4950997ac8fa7cf4ee
aliasferno <aliasferno@gmail.com>
2025-11-10 19:51:30 -0500

    Cambios api dashboard

--- dashboard_app.py
+++ dashboard_app.py
@@ -625,7 +625,7 @@ with tab2:
     api_activa = verificar_api()
     
     if api_activa:
-        st.success(f"✅ API de Segmentacion Conectada URL: `{API_BASE_URL}`")
+        st.success(f"✅ API de Segmentacion Conectada")
     else:
         st.warning("⚠️ API de Segmentacion no disponible. Inicia la API con: `python api_segmentacion.py`")
     
